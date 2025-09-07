5a4adfd5054526470695df814db288ae8c85acdc
Kim Hyo Chan <skkhc0317@gmail.com>
2025-09-07 18:09:01 +0900

    25.09.07. Add study comment

--- backend/src/test/hyochan/lab4/list.php
+++ backend/src/test/hyochan/lab4/list.php
@@ -1,62 +1,59 @@
 <?php
 
     // 페이지네이션
-    $limit = 5;   // 한 페이지에 게시물을 얼마나 출력할 지
-    $page = isset($_GET['page']) ? $_GET['page'] : 1;  // 현재 페이지
-    $offset = ($page - 1) * $limit;  // limit이 정해졌다면 다음 페이지로 이동 시 얼마만큼 뛰어넘을건지
+    $limit = 5;  // 한 페이지 당 게시글 개수
+    $page = isset($_GET['page']) ? $_GET['page'] : 1;
+    $offset = ($page - 1) * $limit;
 
-    // 검색타입, 검색쿼리 유효성 검사
-    // 검색타입: 작성자(name) - 기본
-    // 검색쿼리: 빈 값 - 기
-    $search_type = isset($_GET['search_type']) ? $_GET['search_type'] : 'name';
-    $search_query = isset($_GET['search_query']) ? trim($_GET['search_query']) : '';
+    // 검색 유효성 검사
+    $search_type = isset($_GET['search_type']) ? $_GET['search_type'] : 'title';
+    $search_query = isset($_GET['search_query']) ? $_GET['search_query'] : '';
 
     // 검색 조건
     $where = '';
 
-    // 검색쿼리가 비어있지 않을 경우
+    // 검색 쿼리가 비어있지 않을 경우
     if (!empty($search_query)) {
         $where = "WHERE $search_type LIKE '%$search_query%'";
     }
 
     try {
         // 데이터베이스 연결
-        require_once './db_connect.php';
+        require_once "./db_connect.php";
 
+        // 게시글 목록 출력
         // sql문 작성 (SELECT)
-        $sql = "SELECT * FROM guestbook $where ORDER BY id DESC LIMIT $limit OFFSET $offset";
+        $sql = "SELECT * FROM board $where ORDER BY postID DESC LIMIT $limit OFFSET $offset";
 
         // 쿼리 실행
         $result = $db_conn->query($sql);
 
-        // sql문 작성 (SELECT COUNT)
-        $totalSql = "SELECT COUNT(*) total FROM guestbook $where";
+        // 게시글 개수 세기
+        // sql문 작성 (SELECT COUNT(*))
+        $totalSql = "SELECT COUNT(*) total FROM board $where";
 
         // 쿼리 실행
         $totalResult = $db_conn->query($totalSql);
         $totalRow = $totalResult->fetch_assoc();
-
-        // 전체 게시물의 개수
-        $total = $totalRow['total'];
-
-        // 전체 페이지의 개수
-        $totalPages = ceil($total / $limit);
+        $total = $totalRow['total'];   // 전체 게시글 수
+        $totalPages = ceil($total / $limit);  // 전체 페이지 수
 
         // 블록 설정
-        $pagePerBlock = 5;  // 블록 당 페이지 수
-        $currentBlock = ceil($page / $pagePerBlock);  // 현재 블록
-        $startPage = ($currentBlock - 1) * $pagePerBlock + 1;  // 현재 블록에서 시작 페이지
-        $endPage = min($pagePerBlock * $currentBlock, $totalPages);   // 현재 블록에서 마지막 페이지
+        $pagePerBlock = 5;  // 한 블록 당 페이지 수
+        $currentBlock = ceil($page / $pagePerBlock);   // 현재 블록
+        $startPage = ($currentBlock - 1) * $pagePerBlock + 1;  // 현재 블록 시작 페이지
+        $endPage = min($currentBlock * $pagePerBlock, $totalPages);  // 현재 블록 마지막 페이지
+        $prevBlock = $startPage - 1;  // 이전 블록
+        $nextBlock =  $endPage + 1;   // 다음 블록
 
     } catch (Exception $e) {
         // DB 오류 발생
-        echo "DB 오류 발생<br>".$e;
+        echo "DB 오류 발생";
         exit;
     }
 
     // 데이터베이스 종료
     $db_conn->close();
-
 ?>
 
 <!DOCTYPE html>
@@ -68,35 +65,36 @@
 </head>
 <body>
     <!--
-    방명록 목록
+    게시판 목록
 
     검색타입 검색쿼리 검색버튼
-    Form
-    Action: list.php
-    Method: get
-    입력값: 검색타입(search_type), 검색쿼리(search_query)
 
     <table>
     번호 작성자 제목 작성일
+    postID name title created_at
     </table>
-    
-    글쓰기 버튼 활성화
-    -->
 
-    <h1>방명록 목록</h1>
+    글쓰기 버튼 -> insert.php
+
+    페이지네이션
+    -->
+    <h1>게시판 목록</h1>
 
     <form action="list.php" method="get">
         <select name="search_type">
+            <option value="title">제목</option>
+            <option value="content">내용</option>
             <option value="name">작성자</option>
-            <option value="msg">내용</option>
         </select>
 
-        <input type="search" name="search_query" value="<?= $search_query; ?>">
+        <input type="search" name="search_query">
 
         <button>검색</button>
     </form>
 
-    <table border="1">
+    <br>
+
+    <table border='1'>
         <tr>
             <th>번호</th>
             <th>작성자</th>
@@ -105,72 +103,55 @@
         </tr>
 
         <?php
+            
+            // 게시물이 없을 경우
+            // 게시물이 없습니다.
+            $count = $total;   // 게시물 개수 세기
 
-            // 게시글이 없을 경우
-            // 게시글이 없습니다.
             if ($result->num_rows <= 0) {
-                echo "<tr>";
-                echo "<td colspan=4>게시글이 없습니다.</td>";
-                echo "</tr>";
-            }
-
-            // 게시글이 있을 경우
-            while ($row = $result->fetch_assoc()) {
-                echo "<tr>";
-                echo "<td>$row[id]</td>";
-                echo "<td>$row[name]</td>";
-                echo "<td><a href='view.php?id=$row[id]'>$row[title]</a></td>";
-                echo "<td>$row[created_at]";
-                echo "</tr>";
+                echo "<tr><td colspan='4'>게시물이 없습니다.</td></tr>";
+            } else {   // 게시물이 있을 경우
+                while ($row = $result->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>$row[postID]</td>";
+                    echo "<td>$row[name]</td>";
+                    echo "<td><a href='read.php?id=$row[postID]'>$row[title]</a></td>";
+                    echo "<td>$row[created_at]</td>";
+                    echo "</tr>";
+                }
             }
         ?>
     </table>
 
-    <button onclick="location.href='form.html'">글쓰기</button>
+    <button onclick="location.href='insert.php'">글쓰기</button>
 
     <?php
-        // 검색타입, 검색쿼리 반영
-        $search = "search_type=$search_type&search_query=$search_query";
+        // 검색 결과 반영
+        $search = "&search_type=$search_type&search_query=$search_query";
 
-        // 첫 번째 블록일 경우
-        // <<, < 출력 X
-
-        // 첫 번째 블록이 아닐 경우
-        // <<, < 출력
-        $prevPage = $startPage - 1;
+        // 이전 블록 이동
+        // 현재 블록이 1이 아니면 이전 블록 이동 버튼 출력
         if ($currentBlock != 1) {
-            // 첫 페이지 이동
-            echo "<a href='?page=1&$search'><<</a> ";
-
-            // 이전 블록 이동
-            echo "<a href='?page=$prevPage&$search'><</a> ";
+            echo "<a href='?page=1$search'><<</a> ";
+            echo "<a href='?page=$prevBlock$search'><</a> ";
         }
 
-        // 현재 블록 전체 페이지 출력
-        for ($i = $startPage; $i <= $endPage ; $i++) {
-            // 현재 페이지 굵게
-            if ($i == $page) {
-                echo "<a href='?page=$i&$search'><strong>$i</strong></a> ";
-            } else {  // 현재 페이지 제외하고는 가늘게
-                echo "<a href='?page=$i&$search'>$i</a> ";
+        // 현재 블록 페이지 출력
+        // 현재 페이지는 진하게 출력
+        for ($i = $startPage ; $i <= $endPage ; $i++) {
+            if ($page == $i) {
+                echo "<a href='?page=$i$search'><strong>$i</strong></a> ";
+            } else {
+                echo "<a href='?page=$i$search'>$i</a> ";
             }
         }
 
-        // 전체 페이지 수와 마지막 페이지가 같을 경우
-        // >, >> 출력 x
-
-        // 전체 페이지 수와 마지막 페이지가 같지 않을 경우
-        // >, >> 출력
-        $nextPage = $endPage + 1;
+        // 다음 블록 이동
+        // 전체 페이지와 현재 블록 마지막 페이지가 같지 않으면 다음 블록 이동 버튼 출력
         if ($totalPages != $endPage) {
-            // 다음 블록 이동
-            echo "<a href='?page=$nextPage&$search'>></a> ";
-
-            // 마지막 페이지 이동
-            echo "<a href='?page=$totalPages&$search'>>></a> ";
+            echo "<a href='?page=$nextBlock$search'>></a> ";
+            echo "<a href='?page=$totalPages$search'>>></a> ";
         }
-    
     ?>
-
 </body>
 </html>
--- backend/src/test/hyochan/lab4/review.php
+++ backend/src/test/hyochan/lab4/review.php
@@ -1,15 +1,17 @@
 <?php
 
-    // 입력값 유효성 검사
-    $id = isset($_GET['id']) ? $_GET['id'] : '';
-    $account = isset($_POST['account']) ? $_POST['account'] : '';
-    $pw = isset($_POST['pw']) ? $_POST['pw'] : '';
-    $content = isset($_POST['content']) ? $_POST['content'] : '';
+    // postID값 유효성 검사
+    $postID = isset($_GET['id']) ? $_GET['id'] : '';
 
-    // 유효하지 않는 입력값일 경우,
-    // 잘못된 접근입니다. -> 게시물 읽기 페이지 리다이렉션
-    if (empty($id) || empty($account) || empty($pw) || empty($content)) {
-        header("Refresh: 2; URL='view.php?id=$id'");
+    // 입력값 유효성 검사
+    $name = isset($_POST['name']) ? $_POST['name'] : '';
+    $pw = isset($_POST['pw']) ? $_POST['pw'] : '';
+    $review = isset($_POST['review']) ? $_POST['review'] : '';
+
+    // 유효하지 않은 postID값이나 입력값일 경우
+    // 잘못된 접근입니다. -> 게시판 목록 리다이렉션
+    if (empty($postID) || empty($name) || empty($pw) || empty($review)) {
+        header("Refresh: 2; URL='list.php'");
         echo "잘못된 접근입니다.";
         exit;
     }
@@ -18,21 +20,26 @@
         // 데이터베이스 연결
         require_once "./db_connect.php";
 
+        // 비밀번호 해싱
+        $pw_hash = password_hash($pw, PASSWORD_DEFAULT);
+
         // sql문 작성 (INSERT)
-        $sql = "INSERT INTO comment (id, account, pw, content) VALUES ('$id', '$account', '$pw', '$content')";
+        $sql = "INSERT INTO comment (postID, name, pw, review) VALUES (
+               '$postID', '$name', '$pw_hash', '$review')";
 
         // 쿼리 실행
         $result = $db_conn->query($sql);
 
-        // 입력에 성공했을 경우
-        // 게시물 읽기 페이지 리다이렉션
-        header("Location: view.php?id=$id");
+        // 댓글 작성에 성공했을 경우
+        // 해당 게시글 페이지 리다이렉션
+        header("Location: read.php?id=$postID");
 
     } catch (Exception $e) {
         // DB 오류 발생
-        echo "DB 오류 발생 <br>".$e;
+        echo "DB 오류 발생<br>".$e;
     }
 
     // 데이터베이스 종료
     $db_conn->close();
+
 ?>
