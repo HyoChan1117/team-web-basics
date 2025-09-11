81fa3ba2dcd7307b05ddf25cc6db96a6e8af7a9b
dhinesh1404 <dhineshksrinivasan@gmail.com>
2025-09-11 21:52:29 +0900

    today test

---
+++ backend/src/test/dhinesh/test2/save.php
@@ -0,0 +1,40 @@
+<?php
+
+    // get the user input
+    $name = isset($_POST['Name']) ? $_POST['Name']: '';
+    $messageArea = isset($_POST['messageArea']) ? $_POST['messageArea']: '';
+
+    // validate the input
+    if(empty($name) || empty($messageArea)){
+        header("refresh: 2; URL= 'form.html'");
+        echo "Invaild input please try again";
+        exit;
+    }
+
+    try{
+        // db address
+        require_once "./db_config.php";
+
+        // sql statement
+        $sql = "INSERT INTO guestbook (name, messageArea) VALUES ('$name','$messageArea')";
+
+        // run sql
+        $result = $db_conn->query($sql);
+
+        // display an error message
+        if(!$result) {
+            header("refresh: 2; URL= 'form.html'");
+            echo "Fail to submit";
+            exit;
+        }
+        header("refresh:2; URL= 'list.php'");
+        echo "sucessfully submitted";
+        exit;
+
+    }catch(Exception $e) {
+        // db error 
+        echo "DB error".$e;
+    }
+    // db close
+    $db_conn->close();
+?>
