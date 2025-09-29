5c718d11c47d88ca70d1996c5f01ed0ee7644a11
HARUNA <166793731+TANAKAHARUNA7@users.noreply.github.com>
2025-09-29 23:47:01 +0900

    Haruna (#32)
    * 25.09.07 댓글 추가 토중
    
    * 25.09.08 댓글 기능 수정
    
    * 25.09.10 댓글 기능 태스트
    
    * "25.09.16  링크를 id->postID로 수정"
    
    * "205.09.16 댓글 기능 추가"
    
    * "25,09,16 댓글 기능"
    
    * "25,09,16 search기능 수정
    
    * "25,09,16 삭제 기능 구현"
    
    * "25,09,16 수정"
    
    * 25.9.24 DB作成
    
    * 25.09.24. Update study javaScript
    
    * 25.09.24. Update php's setting
    
    * "25.9.29 회원가입"

---
+++ backend/src/test/haruna/test/db_connect.php
@@ -0,0 +1,11 @@
+<?php
+
+    // 데이터베이스 연결
+    $hostname = "db";
+    $username = "root";
+    $password = "root";
+    $database = "backend";
+
+    $db_conn = new mysqli($hostname, $username, $password, $database);
+
+?>
