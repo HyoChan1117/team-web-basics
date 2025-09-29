7668777e0d45c80b9348a0a84513d535ce6f4653
Kim Hyo Chan <skkhc0317@gmail.com>
2025-09-29 23:19:05 +0900

    25.09.29. Update reservation system

---
+++ backend/src/db_verify/main.php
@@ -0,0 +1,27 @@
+<?php
+    require_once "./header.php";
+?>
+
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>메인페이지</title>
+</head>
+<body>
+
+    <h1>메뉴</h1>
+
+    <ul>
+        <li><a href="salon.php">salon</a></li>
+        <li><a href="service.php">service</a></li>
+        <li><a href="hair_style.php">hair style</a></li>
+        <li><a href="staff.php">staff</a></li>
+        <li><a href="bookings.php">bookings</a></li>
+        <li><a href="news.php">news</a></li>
+        <li><a href="mypage.php">mypage</a></li>
+    </ul>
+    
+</body>
+</html>
