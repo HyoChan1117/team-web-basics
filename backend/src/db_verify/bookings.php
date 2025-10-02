9b2656b3a6f7348465f19f7328487e1c375697ee
HyoChan Kim <skkhc0317@gmail.com>
2025-10-02 11:52:53 +0900

    Hyochan (#36)
    * 25.09.07. Add study comment
    
    * 25.09.07. Distribute lab about comment
    
    * 25.09.08. Update reference file about comment
    
    * 25.09.10. Make board study code
    
    * 25.09.12. Update board db schema
    
    * 25.09.17. Update reservation system & javascript practice
    
    * 25.09.21. Update study reservation system
    
    * 25.09.23. Update make db schema & reservation system
    
    * 25.09.23. Update reservation system
    
    * 25.09.29. Update reservation system
    
    * 25.09.30. Update reservation system's db schema
    
    * 25.10.02. Update reservation system

--- backend/src/db_verify/bookings.php
+++ backend/src/db_verify/bookings.php
@@ -13,34 +13,34 @@
         require_once "./db_connect.php";
 
         // sql문 작성 (SELECT)
-        $sql = "SELECT 
-                r.*,
-                u1.user_name AS client_name,
-                u2.user_name AS designer_name
-                FROM Reservation AS r
-                JOIN Users AS u1
-                    ON r.client_id=u1.user_id
-                JOIN Users AS u2
-                    ON r.designer_id=u2.user_id
-                ORDER BY date, start_at";
+        $sql_to = "SELECT
+                   t.*,
+                   u.user_name AS designer_name
+                   FROM TimeOff AS t
+                   JOIN Users AS u
+                        ON t.designer_id=u.user_id"
+                   ;
+        $sql_rsv = "SELECT 
+                    r.*,
+                    u1.user_name AS client_name,
+                    u2.user_name AS designer_name
+                    FROM Reservation AS r
+                    JOIN Users AS u1
+                        ON r.client_id=u1.user_id
+                    JOIN Users AS u2
+                        ON r.designer_id=u2.user_id
+                    ORDER BY date, start_at";
         $sql_client = "SELECT user_id FROM Users WHERE account='$session_account'";
-        $sql_designer = "SELECT user_id FROM Users WHERE role='designer'";
+        $sql_designer = "SELECT user_id, user_name FROM Users WHERE role='designer'";
 
         // 쿼리 실행
-        $result = $db_conn->query($sql);
+        $result_to = $db_conn->query($sql_to);
+        $result_rsv = $db_conn->query($sql_rsv);
         $result_client = $db_conn->query($sql_client);
         $result_designer = $db_conn->query($sql_designer);
 
         // 고객
         $row_client = $result_client->fetch_assoc();
-
-        // 디자이너
-        $designers = array();
-        $count_d = 0;
-        while ($row = $result_designer->fetch_assoc()) {
-            $designers[$count_d] = $row['user_id'];
-            $count_d++;
-        }
 
     } catch (Exception $e) {
         // DB 오류 발생
@@ -71,13 +71,23 @@
     <table>
         <tr>
             <th>번호</th>
-            <th>예약자</th>
             <th>디자이너</th>
             <th>서비스</th>
             <th>요구사항</th>
             <th>날짜</th>
-            <th>시간</th>
+            <th>시작시간</th>
+            <th>종료시간</th>
             <th>상태</th>
+        </tr>
+    </table>
+
+    디자이너 휴무
+    <table>
+        <tr>
+            <th>번호</th>
+            <th>디자이너</th>
+            <th>시작일</th>
+            <th>종료일</th>
         </tr>
     </table>
 
@@ -93,32 +103,57 @@
     <table border="1">
         <tr>
             <th>번호</th>
-            <th>예약자</th>
             <th>디자이너</th>
             <th>서비스</th>
             <th>요구사항</th>
             <th>날짜</th>
-            <th>시간</th>
+            <th>시작시간</th>
+            <th>종료시간</th>
             <th>상태</th>
         </tr>
 
         <?php
-
-            if ($result->num_rows <= 0) {
+            $count = 1;
+            if ($result_rsv->num_rows <= 0) {
                 echo "<tr><td colspan='8'>예약 현황이 없습니다.</td></tr>";
             } else {
-                while ($row = $result->fetch_assoc()) {
+                while ($row = $result_rsv->fetch_assoc()) {
                     echo "<tr>";
-                    echo "<td>$row[reservation_id]</td>";
-                    echo "<td>$row[client_name]</td>";
+                    echo "<td>$count</td>";
                     echo "<td>$row[designer_name]</td>";
                     echo "<td>$row[service]</td>";
                     echo "<td>$row[requirement]</td>";
                     echo "<td>$row[date]</td>";
                     echo "<td>$row[start_at]</td>";
+                    echo "<td>$row[end_at]</td>";
                     echo "<td>$row[status]</td>";
                     echo "</tr>";
+
+                    $count++;
                 }
+            }
+        ?>
+    </table>
+
+    <br>
+
+    <h3>디자이너 휴무 (휴일: 매주 일요일)</h3>
+    <table border='1'>
+        <tr>
+            <th>번호</th>
+            <th>디자이너</th>
+            <th>시작일</th>
+            <th>종료일</th>
+        </tr>
+
+        <?php
+            while ($row = $result_to->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>$row[to_id]</td>";
+                echo "<td>$row[designer_name]</td>";
+                echo "<td>$row[start_at]</td>";
+                echo "<td>$row[end_at]</td>";
+                echo "</tr>";
             }
         ?>
     </table>
@@ -142,10 +177,9 @@
             <textarea name="requirement" cols="30" rows="5"></textarea>
 
             <p><strong>DESIGNER</strong></p>
-            <input type="radio" id="hyochan" name="designer" value="<?= $designers[0] ?>">
-            <label for="hyochan">김효찬</label>
-            <input type="radio" id="haruna" name="designer" value="<?= $designers[1] ?>">
-            <label for="haruna">하루나</label>
+            <?php while ($row = $result_designer->fetch_assoc()):; ?>
+            <input type="radio" name="designer" value="<?= $row['user_id'] ?>"> <?= $row['user_name'] ?>
+            <?php endwhile; ?>
 
             <p><strong>TIME</strong></p>
             <input type="date" name="date" value="<?= $date ?>">
