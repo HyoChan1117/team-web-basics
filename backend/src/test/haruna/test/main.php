c1a9517ad742fcb3acbef5a68a13ad769de290df
HyoChan Kim <skkhc0317@gmail.com>
2025-10-09 21:17:30 +0900

    Haruna (#39)
    * 25.09.07 댓글 추가 토중
    
    * 25.09.08 댓글 기능 수정
    
    * 25.09.10 댓글 기능 태스트
    
    * "25.09.16  링크를 id->postID로 수정"
    
    * "205.09.16 댓글 기능 추가"
    
    * "25,09,16 댓글 기능"
    
    * "25,09,16 search기능 수정
    
    * "25,09,16 삭제 기능 구현"
    
    * "25,09,16 수정"
    
    * 25.9.24 DB作成
    
    * 25.09.24. Update study javaScript
    
    * 25.09.24. Update php's setting
    
    * "25.9.29 회원가입"
    
    * "25.9.30 예약페이지"
    
    * "25. 9.30  login"
    
    * "25.9.30 logint페이지 작성"
    
    * "25.9.30 Reservation table에 date, requirement colum추가"
    
    * "25.9.30 페이지만 작성"
    
    * "25.9.30 회원 가입 구현"
    
    * "25. 10.1 designer 선텍 기능 구현 "
    
    * "25. 10. 1 잘 값이 넘어 가는지 확인"
    
    * "25.10.5 메뉴 가격 정보 취특 알고리즘 추가"
    
    * "25. 10. 8 ReservationService테이블 추가"
    
    * "25. 10. 8 SESSION에 저장된 이름과 account명을 표시하기"
    
    * "25. 10. 8 로그인 성공시 이름과 account, user_id를 SESSION에 저장하기"
    
    * "25.10.8  예약 정보 INSERT문 수정 도충"
    
    * "25.10.8 datatime->time로 변경"
    
    * 25.10.09. branch test
    
    ---------
    
    Co-authored-by: haruna <haruna0306@g.yju.ac.kr>

--- backend/src/test/haruna/test/main.php
+++ backend/src/test/haruna/test/main.php
@@ -1,3 +1,9 @@
+<?php
+    session_start();
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,9 +11,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>mainpage</title>
 </head>
-<body>
+<body></body>
     <h1>mainpage</h1>
     <hr>
+    <?php if (isset($_SESSION['user_id'])):?>
+            황영합니다!<strong> <?=$_SESSION['name']?></strong>(<?=$_SESSION['account']?>)님✨
+    <?php endif; ?>
+
+    <br>
     <a href="booking.php">예약 하기</a>
 </body>
 </html>
