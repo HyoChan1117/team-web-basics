3fec3ded64f044cb651626129e29e4d806e5725c
Kim Hyo Chan <skkhc0317@gmail.com>
2025-10-19 23:41:37 +0900

    25.10.19. file organization

--- backend/src/db_verify/main.php
+++
@@ -1,9 +0,0 @@
-<?php
-    
-    // 로그인 정보 불러오기
-    require_once "./header.php";
-
-    // 메뉴판 불러오기
-    require_once "./menu.html"
-    
-?>
--- backend/src/db_verify/rsv_cancel.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-    // reservation_id 값 유효성 검사
-    $reservation_id = isset($_POST['reservation_id']);
-
-?>
