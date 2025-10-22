a62a6d72f83f9db1c060658d7678e542e22cbffc
haruna <haruna0306@g.yju.ac.kr>
2025-10-22 23:54:19 +0900

    25.10.22 role에 따라 환영메시지 다르게 표시한는 코드 위치를 수정

--- backend/src/test/haruna/test2/mypage.php
+++ backend/src/test/haruna/test2/mypage.php
@@ -1,6 +1,17 @@
 <?php
     session_start();
+    
+    $msg = "안녕하세요! 환영합니다!";
+    # role에 따라 환영메시지 다르게 표시
+    if (!empty($_SESSION['role'])){
+        require_once("./user.php");
+    } else {
+        header("Refresh: 2; URL='login.php'");
+        echo "잘못한 접근";
+        exit;
+    }
 ?>
+<a href="logout.php">-logout-</a>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -10,14 +21,7 @@
     <title>mypage</title>
 </head>
 <body>
-    <?php
-        $msg = "안녕하세요! 환영합니다!";
-        if (!empty($_SESSION['account'])){
-            require_once("./user.php");
-        } else {
-            echo "잘못한 접근";
-        }
-    ?><a href="logout.php">-logout-</a>
+        <?= $_SESSION['account']. $_SESSION['user_id']. $_SESSION['user_name']. $_SESSION['role'] ; ?>
     <h1>MENU</h1>
     <ul>
         <li><a href="salon.php">salon</a></li>
@@ -29,6 +33,7 @@
         <li><a href="mypage.php">mypage</a></li>
     </ul>
     <h1>MYPAGE</h1>
+    <!-- role에 따라 다른 페이지를 my페이지에 출력 -->
     <?php if($_SESSION['role'] == 'client'):?>
         <?php require_once("./client2.php"); ?>
     <?php elseif ($_SESSION['role'] == 'designer'):?>
