dc215277f1d0b6157dc9229d2dddcabe3c628471
Kim Hyo Chan <skkhc0317@gmail.com>
2025-10-31 01:27:00 +0900

    25.10.31. Update vue study

---
+++ frontend/vue/v02/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+import { ref } from 'vue';
+
+const newText = ref('');
+
+const arr = ref([]);
+
+// arr.value 안에 new.Todo의 값 저장
+function addText()
+{
+const text = newText.value.trim();
+}
+
+
+</script>
+
+
+<template>
+
+</template>
